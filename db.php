0cec1b0cb1e28f7ff9f4b720a6e17f442b6932a2
Zero <skytr9947@gmail.com>
2019-05-23 10:13:41 +0800

    first commit

---
+++ db.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php // define database connection variables as constant
+
+define('DB_USER',"root");//db user
+
+define('DB_PASSWORD',"");// db password
+
+define('DB_DATABASE', "20_samuel_pessdb");// database name
+
+define('DB_SERVER', "localhost");// db server
+
+?>
+
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Police Emergency Service System</h1>
+
+<nav>
+<a href="logcall.php">Log Call</a>
+
+<a href="update.php">Update</a>
+
+<a href="#">Report</a>
+
+<a href="#">History</a>
+</nav>
+
+
+</body>
+</html>
